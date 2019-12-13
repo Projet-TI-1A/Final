76ee48509ed62401d4608f80693eed1ddd95d166
Emma-dupont <57994890+Emma-dupont@users.noreply.github.com>
2019-12-13 11:31:09 +0100

    Create affichage_erreurs.h

---
+++ affichage_erreurs.h
@@ -0,0 +1,6 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+void temps_total(int t, RenderWindow& window)/*temps total de la simulation*/
+
+void temps_erreurs(int temps_exterieur , perte__temps RenderWindow& window) /*temps total de la simulation*/
