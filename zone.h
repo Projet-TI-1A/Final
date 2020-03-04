077755b37ad5d898f3841387b2827395994687ab
Emma-dupont <57994890+Emma-dupont@users.noreply.github.com>
2020-03-04 14:17:04 +0100

    Update zone.h

--- zone.h
+++ zone.h
@@ -1,7 +1,8 @@
 #ifndef ZONE_H
 #define ZONE_H
 
-int zone(int i,int j,int a,int b,int r, int e);
-int remplissage(int ,int ,int ,int ,int , int ,int*);
+int zone(int k,int x,int y,int a,int b,int r,int R, int e);
+int remplissage(int k,int x,int y,int xa,int r,int R, int e,int &aire, int **resul);
+int aire_completee(int aire,int airetotale, int condition80, int confition95);
 
 #endif
