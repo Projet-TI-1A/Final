805dc7c438a014d1b49cfba590ecfab0c9829c90
Emma-dupont <57994890+Emma-dupont@users.noreply.github.com>
2020-03-04 13:43:47 +0100

    Delete affichage_erreurs.h

--- affichage_erreurs.h
+++
@@ -1,6 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
-
-void temps_total(int t, RenderWindow& window);   /*temps total de la simulation*/
-
-void temps_erreurs(int temps_exterieur , perte__temps RenderWindow& window);    /*temps total de la simulation*/
