ed19c940ca30fd9069e720abc06ded10b2d7c3d9
Emma-dupont <57994890+Emma-dupont@users.noreply.github.com>
2020-03-05 16:57:34 +0100

    Update parametre.h

--- parametre.h
+++ parametre.h
@@ -7,8 +7,8 @@
 #define r 150 //rayon du cercle
 #define L 480 //longeur de l'écran
 #define l 320 //largeur de l'écran
-#define xcentre 240 //position du centre du cercle
-#define ycentre 160
+#define xcentre 250 //position du centre du cercle
+#define ycentre 170
 #define condition95 95 //pourcentage minimum a completer pour passer a la zone suivante
 #define condition80 80 //pourcentage minimum a completer avant de colorer pixels isolé
 
