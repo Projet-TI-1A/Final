2a0279cc767872ddf0c1d0f50eef7176c713ae6f
Emma-dupont <57994890+Emma-dupont@users.noreply.github.com>
2020-03-28 19:59:43 +0100

    Create resultat.cpp

---
+++ resultat.cpp
@@ -0,0 +1,33 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include "resultat.h"
+
+
+using namespace std;
+using namespace sf;
+
+
+
+void affichage_erreur(tabpoint tab, tabpoint erreur, RenderWindow& window){
+	//calcul le pourcentage d'incisions a l'interieur de la zone
+	int points=tab.gettaille(), erreurs=erreur.gettaille();
+	int n=points*100/float(points+erreurs);
+	// Conversion de l'entier
+	char nb[4];
+ 	sprintf(nb, "%d", n); 
+ 	Font font;//chargement de la police
+	if (!font.loadFromFile("arial.ttf"))
+		{cout<<"erreur chargement police"<<endl;}
+	Text text;
+	text.setFont(font);
+	text.setString("Pourcentage d'incisions a l'interieur de la zone:");
+	text.setCharacterSize(16);
+	text.setFillColor(Color::White);
+	text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+	window.draw(text);
+	text.setString(nb);
+	text.setPosition(390,0);
+	window.draw(text);
+
+	
+}
