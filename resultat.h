16650051a4b07d550a79bbf0892733d346aaf343
Emma-dupont <57994890+Emma-dupont@users.noreply.github.com>
2020-03-28 20:00:15 +0100

    Create resultat.h

---
+++ resultat.h
@@ -0,0 +1,9 @@
+#ifndef RESULTAT_H
+#define RESULTAT_H
+#include "toucher.h"
+
+using namespace sf;
+using namespace std;
+
+void affichage_erreur(tabpoint tab, tabpoint erreur, RenderWindow& window);
+#endif
